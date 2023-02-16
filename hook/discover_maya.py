64171af81dcee3133a5b0f70f37a518bb7f951ab
Lluis Ftrack <lluis.casals@ftrack.com>
2023-02-16 09:15:23 +0100

    add definitions and modify discover

--- hook/discover_maya.py
+++ hook/discover_maya.py
@@ -42,10 +42,17 @@ def on_launch_pipeline_maya(session, event):
     definitions_plugin_hook = os.getenv("FTRACK_DEFINITION_PLUGIN_PATH")
     plugin_hook = os.path.join(definitions_plugin_hook, 'maya', 'python')
 
+    maya_definitions_path = os.path.join(
+        plugin_base_dir, 'resource', 'definitions'
+    )
+
     # from ftrack_connect_pipeline_maya import _version as integration_version
 
     pipeline_maya_base_data['integration']['env'] = {
-        'FTRACK_EVENT_PLUGIN_PATH.prepend': plugin_hook,
+        'FTRACK_EVENT_PLUGIN_PATH.prepend': os.path.pathsep.join(
+            [plugin_hook, maya_definitions_path]
+        ),
+        'FTRACK_DEFINITION_PATH.prepend': maya_definitions_path,
         'PYTHONPATH.prepend': os.path.pathsep.join(
             [python_dependencies, maya_script_path]
         ),
